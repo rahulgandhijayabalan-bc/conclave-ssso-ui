c87462d8a941ed6caf74fb0509ebbede337b33a8
ajithmuthukumar-bc <ajith.muthukumar@brickendon.com>
2023-03-16 17:03:13 +0530

    ppon changes

--- src/app/pages/manage-organisation/manage-organisation-profile/manage-organisation-profile.component.ts
+++ src/app/pages/manage-organisation/manage-organisation-profile/manage-organisation-profile.component.ts
@@ -53,6 +53,7 @@ export class ManageOrganisationProfileComponent extends BaseComponent implements
     ];
     submitted: boolean = false;
     @ViewChildren('input') inputs!: QueryList<ElementRef>;
+    pponSchema: any;
 
 
 
@@ -100,6 +101,11 @@ export class ManageOrganisationProfileComponent extends BaseComponent implements
             await this.ciiService.getOrgDetails(ciiOrgId).toPromise().then((data: any) => {
                 localStorage.setItem('cii_registries', JSON.stringify(data));
                 this.registries = data;
+                data.additionalIdentifiers.forEach((Identifier:any)=>{
+                 if(Identifier.scheme != this.pponSchema){
+                    this.additionalIdentifiers.push(Identifier)
+                  }   
+                })
                 this.additionalIdentifiers = data.additionalIdentifiers;
             }).catch(e => {
             });
