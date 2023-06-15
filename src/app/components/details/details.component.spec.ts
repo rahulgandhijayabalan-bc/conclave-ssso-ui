e4fc94353c9a81a4de3c9b0591a6c6697e6a7936
ajithmuthukumar-bc <ajith.muthukumar@brickendon.com>
2023-06-15 19:11:49 +0530

    frame work integarion completed

---
+++ src/app/components/details/details.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { DetailsComponent } from './details.component';
+
+describe('DetailsComponent', () => {
+  let component: DetailsComponent;
+  let fixture: ComponentFixture<DetailsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DetailsComponent]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display the correct title', () => {
+    const titleElement = fixture.nativeElement.querySelector('h1');
+    expect(titleElement.textContent).toContain('Component Title');
+  });
+
+  // Write more test cases as needed
+
+});
