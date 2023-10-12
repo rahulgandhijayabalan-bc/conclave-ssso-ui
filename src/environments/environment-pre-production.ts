301e8f9cb3e578bf01f7efa11a8349117e97fc5c
ponselvamsakthivel-bc <87376328+ponselvamsakthivel-bc@users.noreply.github.com>
2023-10-12 19:23:00 +0530

    Update environment-pre-production.ts

--- src/environments/environment-pre-production.ts
+++ src/environments/environment-pre-production.ts
@@ -8,11 +8,11 @@ export const environment = {
 
       api: {
 
-          isApiGateWayEnabled: true,
+          isApiGateWayEnabled: false,
 
-          security: 'https://pre.api.crowncommercial.gov.uk',
+          security: 'https://pre.ppg-sso-service.crowncommercial.gov.uk',
 
-          postgres: 'https://pre.api.crowncommercial.gov.uk/dashboard-wrapper',
+          postgres: 'https://pre.ppg-sso-service.crowncommercial.gov.uk/dashboard-wrapper',
 
 
           wrapper: {
@@ -50,7 +50,7 @@ export const environment = {
 
       web: {
 
-          dashboard: 'https://preprod.identify.crowncommercial.gov.uk',
+          dashboard: 'https://pre-ppg-sso.crowncommercial.gov.uk',
           name: "PRE-PROD"
       },
 
