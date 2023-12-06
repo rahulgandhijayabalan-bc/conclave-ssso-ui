ef6618b7136cbff920efbfe2595e9c8977f49b2f
ponselvamsakthivel-bc <87376328+ponselvamsakthivel-bc@users.noreply.github.com>
2023-12-06 12:15:39 +0530

    Update environment-production.ts

--- src/environments/environment-production.ts
+++ src/environments/environment-production.ts
@@ -7,7 +7,7 @@ export const environment = {
       //security: 'https://dev.security.conclave.crowncommercial.gov.uk',
       security: 'https://api.crowncommercial.gov.uk',
       //postgres: 'https://dev.core.conclave.crowncommercial.gov.uk',
-      postgres: 'https://api.crowncommercial.gov.uk/dashboard-wrapper',
+      postgres: 'https://api.crowncommercial.gov.uk/core',
       //cii: 'https://conclave-cii-integration-brash-shark-mk.london.cloudapps.digital',
       wrapper: {
         apiGatewayEnabled: {
@@ -15,12 +15,14 @@ export const environment = {
           organisation: 'https://api.crowncommercial.gov.uk/organisation-profiles',
           contact: 'https://api.crowncommercial.gov.uk/contacts',
           configuration: 'https://api.crowncommercial.gov.uk/configurations',
+          dataMigration: 'https://api.crowncommercial.gov.uk/datamigration',
         },
         apiGatewayDisabled: {
           user: 'https://prod-api-wrapper.london.cloudapps.digital/users',
           organisation: 'https://prod-api-wrapper.london.cloudapps.digital/organisations',
           contact: 'https://prod-api-wrapper.london.cloudapps.digital/contacts',
           configuration: 'https://prod-api-wrapper.london.cloudapps.digital/configurations',
+          dataMigration: 'https://prod-api-wrapper.london.cloudapps.digital/datamigration',
         }
       }
     },
@@ -30,7 +32,7 @@ export const environment = {
       name: "",   // <= name should not be any value in production
     },
     ccsContactUrl: "https://www.crowncommercial.gov.uk/contact",
-    ccsDashboardUrl: "https://www.crowncommercial.gov.uk"
+    ccsDashboardUrl: "https://identify.crowncommercial.gov.uk"
   },
   googleTagMangerId: 'GTM',
   cookieExpirationTimeInMinutes: 525600,
@@ -73,5 +75,7 @@ export const environment = {
    hideAutoValidation:false,
    hideSimplifyRole:false,
    blockedScheme: ["GB-PPG"],
+   isMaintenance: true,    
+   customMfaEnabled : false,
   },
 };
